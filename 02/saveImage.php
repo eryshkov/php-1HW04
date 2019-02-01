f6c8a749726f29527080fe7662222d55daf4b8d9
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-01 22:15:01 +0300

    task 2 is completed

--- 02/saveImage.php
+++ 02/saveImage.php
@@ -1,5 +1,6 @@
 <?php
 $isSuccess = false;
+$userImageName = '';
 
 if (isset($_FILES['image'])) {
     $savedImage = $_FILES['image'];
@@ -7,8 +8,11 @@ if (isset($_FILES['image'])) {
     if (0 === $savedImage['error']) {
         $savedImagePath = $savedImage['tmp_name'];
         $userImageName = $savedImage['name'];
-        $isSuccess = move_uploaded_file($savedImagePath, __DIR__ . '/img/' . $userImageName);
-        var_dump($savedImage);
+        $imageMimeType = $savedImage['type'];
+        $isImage = strpos($imageMimeType, 'image') === 0;
+        if ($isImage) {
+            $isSuccess = move_uploaded_file($savedImagePath, __DIR__ . '/img/' . $userImageName);
+        }
     }
 }
 
@@ -16,6 +20,8 @@ if ($isSuccess) {
     header('Location:' . '/02/');
     http_response_code(302);
     exit;
-} else {
-    echo 'Что-то пошло не так';
+} elseif (!$isImage) {
+    ?>Загруженный файл не является изображением<?php
+}else{
+    ?>Не удалось сохранить файл <?php echo $userImageName;?> на сервере<?php
 }
