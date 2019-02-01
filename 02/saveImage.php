4d5da730cdfeb56eeb165c3bc06e647586d51c3f
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-01 22:02:31 +0300

    task 2 is fixed

--- 02/saveImage.php
+++ 02/saveImage.php
@@ -1,2 +1,21 @@
 <?php
+$isSuccess = false;
 
+if (isset($_FILES['image'])) {
+    $savedImage = $_FILES['image'];
+
+    if (0 === $savedImage['error']) {
+        $savedImagePath = $savedImage['tmp_name'];
+        $userImageName = $savedImage['name'];
+        $isSuccess = move_uploaded_file($savedImagePath, __DIR__ . '/img/' . $userImageName);
+        var_dump($savedImage);
+    }
+}
+
+if ($isSuccess) {
+    header('Location:' . '/02/');
+    http_response_code(302);
+    exit;
+} else {
+    echo 'Что-то пошло не так';
+}
